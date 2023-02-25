ae2c0bdfb2afbf19f9c0a44887aa75fd5d08e856
Vanessa Natus <vanessa.natus@gmail.com>
2023-02-25 16:55:26 +0200

    add week6 code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+string hello = " we are the champions? ";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();   
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+//int wordCounter = 1;
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }               
+//}
+
+//Console.WriteLine($"Word count int {hello}: {wordCounter} words. ");
